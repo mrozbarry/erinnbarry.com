42ac4bca8a58d5edba2861a51c3861081f7c165a
Alex Barry <alex.barry@gmail.com>
2017-08-13 16:00:27 -0400

    Fixed misaligned icons.

--- src/sections/cv_highlights.js
+++ src/sections/cv_highlights.js
@@ -20,19 +20,20 @@ function CvHighlights (props) {
 function flattenSections (sections) {
   return sections.reduce((items, section) => {
     const side = items[items.length - 1] == "left" ? "right" : "left"
-    const children = flattenChildren(section.children, side)
+    const children = flattenChildren(section.children, side, section.icon)
     return items
       .concat({ section: section.section, type: "section" })
       .concat(children)
   }, [])
 }
 
-function flattenChildren (children, startSide) {
+function flattenChildren (children, startSide, icon) {
   let side = startSide
   return children.map((child, idx) => {
     const item = {
       type: "event",
       side: side,
+      icon: icon,
       title: child.title,
       dates: child.dates,
       subtitle: child.subtitle,
@@ -49,33 +50,41 @@ function listItems (items) {
   return items.map((item, idx) => {
     switch (item.type) {
       case "section":
-        return CvHighlightSection(idx, item.section)
+        return cvHighlightSection(idx, item.section)
         break
 
       case "event":
-        return CvHighlightEvent(idx, item)
+        return cvHighlightEvent(idx, item)
         break
     }
   })
 }
 
 
-function CvHighlightSection (idx, section) {
+function cvHighlightSection (idx, section) {
   return (
     <li key={idx} className="highlight-section">{section}</li>
   )
 }
 
 
-function CvHighlightEvent (idx, event) {
+function cvHighlightEvent (idx, event) {
+	let title = event.title
+	const titleLines = title.split("\n")
+	if (titleLines.length > 1) {
+		title = titleLines.map((line, lineIdx) => {
+			return <div key={lineIdx}>{line}</div>
+		})
+	}
+
   return (
     <li key={idx} className={`highlight highlight--${event.side}`}>
       <div className="highlight-wrapper">
         <div className="highlight-icon">
-          <span className="fa fa-calendar-o" />
+          <span className={`fa fa-${event.icon}`} />
         </div>
         <div className="highlight-content">
-          <div className="highlight-title">{event.title}</div>
+          <div className="highlight-title">{title}</div>
           <div className="highlight-dates">{event.dates}</div>
           <div className="highlight-subtitle">{event.subtitle}</div>
           <div className="highlight-description">{event.description}</div>
